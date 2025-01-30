3efdc641b11a8c559ab8fd5c17854ed13e0c7ee3
hananeltassa <hananeltassa15@gmail.com>
2025-01-30 13:48:21 +0200

    Added genreIcons

---
+++ app/src/utils/genreIcons.js
@@ -0,0 +1,14 @@
+const genreIcons = {
+    Science: "🧪",
+    History: "📜",
+    Football: "⚽",
+    Movies: "🎥",
+    Math: "➕",
+    Geography: "🌍",
+    Technology: "👩‍💻",
+    Literature: "📖",
+    Music: "🎼",
+    Sports: "🏅",
+};
+  
+export default genreIcons;
